b57caac3c8fedf224106241f3e9e63535c65fea1
WilliamRobsahm <william.robsahm@elev.ga.ntig.se>
2023-11-07 19:08:50 +0100

    Made site a little prettier

--- site/scripts/index.js
+++ site/scripts/index.js
@@ -13,9 +13,15 @@ API.Countries.getList().then(result => {
 API.Artists.getList().then(result => {
     if(result.success) {
         result.data.forEach(artist => {
-            $("#artists-temp").append($("<p>").text(artist.name));
+            const $row = $("<tr>");
+            $row.append($("<td>").append(artist.name));
+            $row.append($("<td>").append(artist.years_active));
+            $row.append($("<td>").append(artist.description));
+            $("#artists").append($row);
         })
     }
 })
 
-// API.Artists.save({ id: 1, name: "100 gecs", years_active: "2015-present", description: "Collaborative project by Laura Les and Dylan Brady." });
+//API.Artists.save({ id: 1, name: "100 gecs", years_active: "2015-present", description: "Collaborative project by Laura Les and Dylan Brady." });
+//API.Artists.save({ name: "２８１４", years_active: "2014-present", description: "Collaborative project by 't e l e p a t h テレパシー能力者'  and 'HKE'" });
+//API.Artists.save({ name: "Test", years_active: "test-present" });
